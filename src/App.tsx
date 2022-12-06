234562a69ea6d4b1192536ea797898bad69ee4bd
Tiago Sousa <contatostiago@gmail.com>
2022-12-06 16:39:11 -0300

    started record.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header';
+import { Record } from './components/Record';
 import './global.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Record />
     </div>
   )
 }
---
+++ src/components/Record.tsx
@@ -0,0 +1,14 @@
+export function Record() {
+  return(
+    <div>
+      <form action="">
+        <input type="text" />
+        <button></button>
+      </form>
+      <footer>
+        <div><p>Tarefas criadas</p><span>0</span></div>
+        <div><p>Concluídas</p><span>0</span></div>
+      </footer>
+    </div>
+  )
+}
