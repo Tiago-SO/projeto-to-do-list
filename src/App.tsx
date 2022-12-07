6bfaacf65d0025378f06ec586e3f06f1fa79c812
Tiago Sousa <contatostiago@gmail.com>
2022-12-07 15:33:51 -0300

    Ending Record

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Header } from './components/Header';
 import { Record } from './components/Record';
 import './global.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
       <Record />
+
+      </div>
     </div>
   )
 }
--- src/components/Record.tsx
+++ src/components/Record.tsx
@@ -1,13 +1,16 @@
+import styles from './Record.module.css';
+import plusButton from '../assets/plus-button.svg';
+
 export function Record() {
   return(
-    <div>
-      <form action="">
-        <input type="text" />
-        <button></button>
+    <div className={styles.record}>
+      <form action="" >
+        <input type="text" placeholder='Adicione uma nova tarefa'/>
+        <button><span>Criar</span><img src={plusButton} alt="" /></button>
       </form>
       <footer>
-        <div><p>Tarefas criadas</p><span>0</span></div>
-        <div><p>Concluídas</p><span>0</span></div>
+        <div><p>Tarefas criadas</p><span className={styles.counter}>0</span></div>
+        <div><p>Concluídas</p><span className={styles.counter}>0</span></div>
       </footer>
     </div>
   )
